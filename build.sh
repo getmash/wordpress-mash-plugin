4f1c54623ce9e26b01bb1b9d7a4a208d67a1108e
Mike Dedys <mdedys@users.noreply.github.com>
2022-08-16 13:24:19 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf dist
 mkdir dist
-zip -R ./dist/mash-wallet-plugin.zip 'assets/*' 'css/*' 'images/*' 'includes/*' 'js/*' 'LICENSE' 'readme.txt' 'mash.php'
+zip -R ./dist/mash-wallet-plugin.zip 'assets/*' 'css/*' 'images/*' 'includes/*' 'js/*' 'LICENSE' 'readme.txt' 'mash.php' 'uninstall.php'
 echo 'Plugin Built'
